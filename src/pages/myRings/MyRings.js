83e62ce3f66e656fc479d6739e643681daf7e76b
Amanda Jose <amanda-jose@live.com>
2022-01-16 09:24:14 -0500

    Update MyRings.js

--- src/pages/myRings/MyRings.js
+++ src/pages/myRings/MyRings.js
@@ -1,24 +1,23 @@
 import './MyRings.css'
 import {useCollection} from '../../hooks/useCollection'
 
+
 export default function MyRings() {
     const {documents, error} = useCollection('rings')
     return (
-        <div>
+        <div className="auth-form">
+            <div className="btns">
+                <button>Back</button>
+                <button>Add</button>
+            </div>
             <h2>My Rings</h2>
             {error && <p className="error">{error}</p>}
             {documents && documents.map(doc=> (
-                <div>
-                <p key={doc.id}>{doc.name}</p>
-                <img key={doc.id} src={doc.image} alt="image"/>
+                <div className="stack">
+                    <p key={doc.id}>{doc.name}</p>
+                    <img className="imgControl" key={doc.id} src={doc.image} alt="image"/>
                 </div>
             ))}
-
-            <button>Back</button>
-            <button>Add</button>
-            <div>
-
-            </div>
         </div>
     )
 }
